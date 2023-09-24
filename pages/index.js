2080a8bdb9e79266b20d26ccaec62a82d88e8bf7
mariamnozadze <mariamnozadze97@yahoo.com>
2023-09-24 16:45:45 +0400

    - Created a dummy banner

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 const Home = () => {
   return (
     <>
-      Herobanner
+      <HeroBanner/>
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>speakers of many variations</p>
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="products-container">
         {["product 1", "product 2"].map((product) => product)}
       </div>
-      footer
+      <FooterBanner/>
     </>
   );
 };
